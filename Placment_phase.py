dbccd36073fa8ade3082d28064aa5c1c4a329179
Patrykk5228 <104195930+Patrykk5228@users.noreply.github.com>
2022-05-24 17:54:16 +0200

    + display_board

--- Placment_phase.py
+++ Placment_phase.py
@@ -10,5 +10,17 @@ def print_board(board):
     for row in board:
         print((" ").join(row))
 
-print("Let's play Battleship!")
+print("Let's play Battleship!\n")
 print_board(board)
+
+def display_board():
+
+    row_list = ("A", "B", "C", "D", "E")
+    index = 0
+    for row in board:
+        if index < 1:
+            print('   ' + "1" + '   ' + "2 " + '  ' + "3" + '   ' + "4" + '   ' + "5\n")
+        print(row_list[index] + ' ' + ' ' + row[0] + '   ' + row[1] + '   ' + row[2] + '   ' + row[3] + '   ' + row[4])
+        index += 1
+        print('  '+(''*(len(board)-2))+'')
+display_board()
