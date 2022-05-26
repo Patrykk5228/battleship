5b16e467f036c844c0c9b08f49e28f78d3aa8a56
Patrykk5228 <104195930+Patrykk5228@users.noreply.github.com>
2022-05-26 20:20:37 +0200

    validation

--- Placment_phase.py
+++ Placment_phase.py
@@ -1,19 +1,29 @@
 from random import randint
+import sys
 
 board = []
+player = input("Podaj nick")
+is_ai = False
+already_chosen = []
 
-def print_board(board):
+
+def get_board():
+
+    board = []
 
     for x in range(5):
         board.append(["O"] * 5)
 
     for row in board:
         print((" ").join(row))
+    return board
 
 print("Let's play Battleship!\n")
-print_board(board) # only for check
+get_board() # only for check
 
-def display_board():
+def display_board(board):
+
+    board = get_board()
 
     row_list = ("A", "B", "C", "D", "E")
     index = 0
@@ -23,5 +33,63 @@ def display_board():
         print(row_list[index] + ' ' + ' ' + row[0] + '   ' + row[1] + '   ' + row[2] + '   ' + row[3] + '   ' + row[4])
         index += 1
         print('  '+(''*(len(board)-2))+'')
+
+def player_turn(player):
+    round = 0
+    if player == 1:
+        round += 1
+    return round
+    pass 
+
+def is_correct_letter(userInput):
+    entered_letter = userInput[0].upper()
+    if entered_letter.isalpha() == False:
+        return False
+    return True
+ 
+def is_number(userInput):
+    try:
+        int(userInput)
+    except ValueError:
+        return False
+    return True
+
+def check_coordinates(player, is_ai, already_chosen):
+
+    round = player_turn(player)
+    error_msg = "Invalid input"
+    while True:
         
-display_board() # only for check
+        if round % 2 == 0 or is_ai:
+            user_input = input('Player 1 please enter coordinates: ')
+        else:
+            user_input = input('Player 2 please enter coordinates: ')
+        if len(user_input) != 2:
+            if user_input == "3":
+                print("Bye Bye")
+                sys.exit()
+            else:
+                print(error_msg)
+                continue
+        isLetter = is_correct_letter(user_input[0])
+        isNumber = is_number(user_input[1])
+        try:
+            coordinate = (user_input[0].upper(), int(user_input[1]))
+            if isLetter is False or isNumber is False:
+                print(error_msg)
+                continue
+        except:
+            print(error_msg)
+            continue
+        if coordinate not in already_chosen:
+            already_chosen.append(coordinate)
+        else:
+            print("This coordinate is already occupied!")
+            continue
+        return coordinate
+
+check_coordinates(player, is_ai, already_chosen)
+
+
+        
+display_board(board) # only for check
